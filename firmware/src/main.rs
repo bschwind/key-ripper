6b05bda2e4dcf76026d6f9d6a6734b1b519f41ce
Brian Schwind <brianmschwind@gmail.com>
2022-07-07 18:24:15 +0900

    Fix matrix mapping formatting, add missing keyboard row

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -35,20 +35,20 @@ const NUM_ROWS: usize = 6;
 
 #[rustfmt::skip]
 const MATRIX_MAPPING: [[KeyCode; NUM_ROWS]; NUM_COLS] = [
-    [KeyCode::Escape,KeyCode::Tilde,KeyCode::Tab,KeyCode::CapsLock,KeyCode::LeftShift,KeyCode::Fn],
+    [KeyCode::Escape, KeyCode::Tilde, KeyCode::Tab, KeyCode::CapsLock, KeyCode::LeftShift, KeyCode::Fn],
     [KeyCode::F1, KeyCode::Num1, KeyCode::Q, KeyCode::A, KeyCode::Empty, KeyCode::LeftCtrl],
     [KeyCode::F2, KeyCode::Num2, KeyCode::W, KeyCode::S, KeyCode::Z, KeyCode::LeftAlt],
     [KeyCode::F3, KeyCode::Num3, KeyCode::E, KeyCode::D, KeyCode::X, KeyCode::LeftCmd],
     [KeyCode::F4, KeyCode::Num4, KeyCode::R, KeyCode::F, KeyCode::C, KeyCode::Empty],
     [KeyCode::F5, KeyCode::Num5, KeyCode::T, KeyCode::G, KeyCode::V, KeyCode::Empty],
-    [KeyCode::Empty, KeyCode::Num6, KeyCode::Y, KeyCode::H, KeyCode::B, KeyCode::Space],
+    [KeyCode::Empty, KeyCode::Num6, KeyCode::Y, KeyCode::H, KeyCode::B, KeyCode::Space], // col 6, row 5
     [KeyCode::F6, KeyCode::Num7, KeyCode::U, KeyCode::J, KeyCode::N, KeyCode::Empty],
     [KeyCode::F7, KeyCode::Num8, KeyCode::I, KeyCode::K, KeyCode::M, KeyCode::Empty],
     [KeyCode::F8, KeyCode::Num9, KeyCode::O, KeyCode::L, KeyCode::Comma, KeyCode::Empty],
-    [KeyCode::F9,KeyCode::Num0,KeyCode::P,KeyCode::Semicolon,KeyCode::Period,KeyCode::RightCmd],
-    [KeyCode::F10,KeyCode::Minus,KeyCode::LeftSquareBracket,KeyCode::SingleQuote,KeyCode::ForwardSlash,KeyCode::Left],
-    [KeyCode::F11,KeyCode::Equals,KeyCode::RightSquareBracket,KeyCode::Enter,KeyCode::Up,KeyCode::Down],
-    [KeyCode::F12,KeyCode::Backspace,KeyCode::BackSlash,KeyCode::Empty,KeyCode::Empty,KeyCode::Right],
+    [KeyCode::F9, KeyCode::Num0, KeyCode::P, KeyCode::Semicolon, KeyCode::Period, KeyCode::RightCmd],
+    [KeyCode::F10, KeyCode::Minus, KeyCode::LeftSquareBracket, KeyCode::SingleQuote, KeyCode::ForwardSlash, KeyCode::Left],
+    [KeyCode::F11, KeyCode::Equals, KeyCode::RightSquareBracket, KeyCode::Enter, KeyCode::Up, KeyCode::Down],
+    [KeyCode::F12, KeyCode::Backspace, KeyCode::BackSlash, KeyCode::Empty, KeyCode::Empty, KeyCode::Right],
 ];
 
 const EXTERNAL_CRYSTAL_FREQUENCY_HZ: u32 = 12_000_000;
@@ -117,6 +117,7 @@ fn main() -> ! {
         &pins.gpio27.into_pull_down_input(),
         &pins.gpio28.into_pull_down_input(),
         &pins.gpio15.into_pull_down_input(),
+        &pins.gpio24.into_pull_down_input(),
     ];
 
     let cols: &mut [&mut dyn OutputPin<Error = Infallible>] = &mut [
